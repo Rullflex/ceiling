ecb103e6463f738610bd641d3eab21491d5da2d1
Dmitry <libafer99@gmail.com>
2021-03-14 11:12:59 +0500

    production

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -151,7 +151,6 @@ document.addEventListener(`DOMContentLoaded`, function () {
         
     }
 
-
     UIkit.scrollspy(`.s13__map`)
     document.querySelector(`.s13__map`).addEventListener(`inview`, (event) => {
         document.querySelector(`.s13__map-script`).insertAdjacentHTML(`beforeend`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1542.5067930807736!2d32.06801302015278!3d49.44337410905408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d14b7c811c873d%3A0x6052c6a008731945!2z0YPQuy4g0KHQvNC10LvRj9C90YHQutCw0Y8sIDE1LCDQp9C10YDQutCw0YHRgdGLLCDQp9C10YDQutCw0YHRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0KPQutGA0LDQuNC90LAsIDE4MDAx!5e0!3m2!1sru!2sru!4v1612765520263!5m2!1sru!2sru" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`)
@@ -163,5 +162,4 @@ document.addEventListener(`DOMContentLoaded`, function () {
         document.querySelector(`#v-about .popup__body`).insertAdjacentHTML(`beforeend`, `<iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/lpGr5OuS4mg" frameborder="0" allowfullscreen="true" data-uk-video data-uk-responsive"></iframe>`)
     })
     
-
 })
